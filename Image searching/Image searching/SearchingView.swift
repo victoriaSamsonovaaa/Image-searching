85c9e1c6c94975a7de250faab80a7ce696f70828
VIKTORYIA SAMSONAVA <viktoriya09june@icloud.com>
2024-07-27 20:03:26 +0300

    edited searching view

--- Image searching/Image searching/SearchingView.swift
+++ Image searching/Image searching/SearchingView.swift
@@ -11,51 +11,122 @@ import SwiftUI
 struct SearchingView: View {
     @State var search = ""
     @State private var model = Model()
+    @State var flag = false
+    @State var showFind = true
     
     var body: some View {
         NavigationStack {
             VStack(spacing: 0) {
                 Spacer()
                     .frame(height: UIScreen.main.bounds.size.height - 830)
-                HStack(spacing: 10) {
-                    TextField("Enter a query to find an image", text: self.$search)
-                        .padding(20)
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .padding(.leading, 10)
+                        .foregroundColor(.secondary)
+                    TextField("Search", text: self.$search)
                         .font(.title3)
-                        .frame(width: UIScreen.main.bounds.size.width - 90, height: 50)
-                        .background(Color.gray.opacity(0.10))
-                        .cornerRadius(10)
-                    Button(action: {
-                        //searchig function
-                        model.results = []
-                        model.searchImages(queryWord: search)
-                    }) {
-                        Image(systemName: "magnifyingglass")
-                            .foregroundColor(.white)
-                            .imageScale(.large)
-                            .frame(width: UIScreen.main.bounds.size.width - 340, height: 50)
-                            .background(Color.blue)
-                            .cornerRadius(10)
+                    
+                    if self.search != "" {
+                        if self.showFind {
+                            Text("Find")
+                                .foregroundColor(.blue)
+                                .padding(.trailing, 10)
+                                .onTapGesture {
+                                    withAnimation {
+                                        self.flag = true
+                                        self.showFind = false
+                                    }
+                                }
+                        }
+                        Image(systemName: "xmark")
+                            .foregroundColor(.red)
+                            .font(.system(size: 15, weight: .bold))
+                            .padding(.trailing, 10)
+                            .onTapGesture {
+                                self.showFind = true
+                                search = ""
+                                self.showFind = true
+                            }
                     }
                 }
-                .foregroundStyle(.tint)
+                .frame(width: UIScreen.main.bounds.size.width - 20, height: 40)
+                .background(Color.gray.opacity(0.10))
+                .cornerRadius(10)
+                .onTapGesture {
+                    self.flag = true
+                }
                 Spacer()
+                    .frame(height: UIScreen.main.bounds.size.height - 830)
                 ScrollView {
-                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 150))]) {
-                        ForEach(model.results) { image in
-                            if let url = URL(string: image.urls.regular) {
-                                AsyncImage(url: url) { image in
-                                    image
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                }
-                                placeholder: {
-                                    ProgressView()
-                                }
-                            }
-                        }
+                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 200))]) {
+//                        ForEach(model.results) { image in
+//                            if let url = URL(string: image.urls.regular) {
+//                                AsyncImage(url: url) { image in
+//                                    image
+//                                        .resizable()
+//                                        .aspectRatio(contentMode: .fit)
+//                                }
+//                                placeholder: {
+//                                    ProgressView()
+//                                }
+//                            }
+//                        }
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
+                        Text("aaaaaaaaaaaaaaaaaaaaa")
                     }
                 }
             }
         }
     }
+}
+
+
+#Preview {
+    ContentView()
 }
