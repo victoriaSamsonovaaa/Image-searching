7fba55d0a7b505e3891b43478f79aa689cc1546f
VIKTORYIA SAMSONAVA <viktoriya09june@icloud.com>
2024-07-29 01:33:02 +0300

    solving problems with API key

--- Image searching/Image searching/Model.swift
+++ Image searching/Image searching/Model.swift
@@ -11,6 +11,17 @@ class Model: ObservableObject {
     
     @Published var results: [Result] = []
     @Published var noImages = false
+    private let apiKey: String
+    
+    init() {
+        // Получаем API ключ из Info.plist
+        if let apiKey = Bundle.main.object(forInfoDictionaryKey: "APIKey") as? String {
+            self.apiKey = apiKey
+            print("got")
+        } else {
+            fatalError("APIKey not found in Info.plist")
+        }
+    }
 
     func searchImages(queryWord: String) {
         //extra
@@ -18,8 +29,8 @@ class Model: ObservableObject {
             self.results.removeAll()
             self.noImages = false
         }
-        let key = ""
-        let urlQuery = "https://api.unsplash.com/search/photos?page=1&per_page=20&query=\(queryWord)&client_id=\(key)"
+        
+        let urlQuery = "https://api.unsplash.com/search/photos?page=1&per_page=20&query=\(queryWord)&client_id=\(apiKey)"
         guard let url = URL(string: urlQuery) else {
             return
         }
@@ -27,10 +38,11 @@ class Model: ObservableObject {
             guard let data = data, error == nil else {
                 return
             }
-                        do {
+            
+            do {
                 let jsonResult = try JSONDecoder().decode(APIResponse.self, from: data)
                 DispatchQueue.main.async {
-                    if jsonResult.results.count == 0 {
+                    if jsonResult.results.isEmpty {
                         self?.noImages = true
                     }
                     else {
@@ -43,4 +55,6 @@ class Model: ObservableObject {
             }
         }.resume()
     }
+    
 }
+
